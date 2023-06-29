15fa6ddc9111515ee00748096c5d85a836f18621
akashsinha0641 <akash.sinha@eventdesu.com>
2023-06-29 15:40:41 +0530

    logo change

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import download from './download.png';
 import logo from './logo.svg';
 import preview from './preview.png';
+import logoNew from './logoNew.png';
 
 export {
   download,
   logo,
   preview,
+  logoNew
 };
